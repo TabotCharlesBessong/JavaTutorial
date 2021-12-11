0892713d0bb5f755b0977aa933e6d80ebf7dc5b9
Tabot Chares Bessong <ebezebeatrice@gmail.com>
2021-12-11 09:27:37 +0100

    first object in java

--- JavaTutorial/CHAPTER 9 BJECT CLASS/Time1.java
+++ JavaTutorial/CHAPTER 9 BJECT CLASS/Time1.java
@@ -5,7 +5,18 @@ public class Time1 {
 
   public void setTime(int hour , int minute , int second){
     if(hour < 0 || hour >= 24 || minute < 0 || minute >= 60 || second < 0 || second >= 60){
-      throw new IllegalArgumentException("Hour minutes and second are out of range")
+      throw new IllegalArgumentException("Hour minutes and second are out of range");
     }
+    this.hour = hour;
+    this.minute = minute;
+    this.second = second;
+  }
+  public String toUniversalString(){
+    return String.format("%02d:%02d:%02d",hour ,minutes , second);
+  }
+  public String toString(){
+    return String.format("%d:%02d:%02d %s",((
+      hour == 0 || hour == 12  )? 12 : hour % 12),
+      minute , second , (hour == 12? "AM":"PM"));
   }
 }
---
+++ JavaTutorial/CHAPTER 9 BJECT CLASS/Time1Test.java
@@ -0,0 +1,26 @@
+public class Time1Test {
+  public static void main(String[] args){
+    //create and innitialise Time 1 object
+    Time1 time = new Time1();
+ 
+    // output string representation of time
+    displayTime("After time object is created ",time);
+    System.out.println();
+
+    // change time and output time 
+    time.setTime(13,27,6);
+    displayTime("After calling setTime",time);
+    System.out.println();
+
+    try {
+      time.setTime(99,99,99);
+    } catch (IllegalArgumentException e) {
+      //TODO: handle exception
+      System.out.printf("Exception: %s%n%n", e.getMessage());
+    }
+    displayTime("After cslling invalid values",time);
+  }
+  private static void displayTime(String header , Time1 t){
+    System.out.printf("%s%nUniversal time : %s%nStandard time : %s%n", header, t.toUniversalString(), t.toString());
+  }
+}
