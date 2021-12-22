d386c54a6f9c2d268893ece3c7e511c641a2c3c5
Tabot Chares Bessong <ebezebeatrice@gmail.com>
2021-12-22 23:02:31 +0100

    WORKING WITH SOME MORE DATA

--- JavaTutorial/CHAPTER 9 BJECT CLASS/Date.java
+++ JavaTutorial/CHAPTER 9 BJECT CLASS/Date.java
@@ -9,7 +9,20 @@ public class Date {
     if(month <= 0 || month > 12)
       throw new IllegalArgumentException("month(" + month + ") must be 1-12");
     //check if month is in range
-    if(month <= 0 || month > 12)
-      throw new IllegalArgumentException("month(" + month + ")must be 1-12");
+    if(day <= 0 || (day > daysPerMonth[month] && !(month ==2 && day == 29) ) )
+      throw new IllegalArgumentException("day(" + day + ") out of range for the specified month of the year");
+      //check if month is in range
+    if(month == 2 && day ==29 && !(year % 400 == 0 || (year % 4 == 0 || year % 100 == 0 )) )
+      throw new IllegalArgumentException("month(" + month + ") must be 1-12");
+
+    this.month = month ;
+    this.day = day;
+    this.year = year;
+
+    System.out.printf("Date object constructor for date %s%n", this);
+
+  }
+  public String toString(){
+    return String.format("%d/%d/%d", month, day ,year);
   }
 }
